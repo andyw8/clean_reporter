0067279aa215b65ef0f06bf8bfc02181dd06bc04
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-03-21 16:48:49 -0400

    Basic setup

--- lib/clean_reporter.rb
+++ lib/clean_reporter.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "clean_reporter/version"
+require "minitest/reporters"
 
 module CleanReporter
   class Error < StandardError; end
   # Your code goes here...
 end
+
+Minitest::Reporters.use!([CleanReporter::Reporter.new(color: true)])
